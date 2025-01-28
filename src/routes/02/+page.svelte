b1329293b25c626688fea2da5adcb48eef4b1987
fraancesss <guilhermefranca438@gmail.com>
2025-01-28 02:49:42 -0400

    conhecimento7

---
+++ src/routes/02/+page.svelte
@@ -0,0 +1,31 @@
+<body>
+    <style>
+        body {
+            background-image: url('https://images3.alphacoders.com/237/237345.jpg');
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+            height: 90vh;
+        }
+        .caixa2 {
+            background-color: rgba(9, 1, 1, 0.5);
+            display: flex;
+            justify-content: center;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+            color: #ff1100 ;
+            width: 250px;
+           
+           
+        }
+        .letra {
+          color:#002de0;
+          font-size:30px;
+          font-weight: bold;
+         
+
+
+        }
+    </style>
+</body>
---
+++ src/routes/03/+page.svelte
@@ -0,0 +1,30 @@
+<body>
+    <style>
+        body {
+            background-image: url('https://images6.alphacoders.com/136/1361763.jpeg');
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+            height: 90vh;
+        }
+        .caixa2 {
+            background-color: rgba(9, 1, 1, 0.5);
+            display: flex;
+            justify-content: center;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+            color: #ff1100 ;
+            width: 250px;
+           
+           
+        }
+        .letra {
+          color:#fa1100;
+          font-size:30px;
+          font-weight: bold;
+
+
+        }
+    </style>
+</body>
