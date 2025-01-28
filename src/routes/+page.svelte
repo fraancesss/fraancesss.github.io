de7553ea97e930d8248e1d5299b2cf8f1f8c4485
fraancesss <guilhermefranca438@gmail.com>
2025-01-28 02:22:17 -0400

    Plano de fundo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Bom dIA</h1>
 
+
+<center>
+    <div class="caixa1">
+     
+    <h1>OLAAA , Beleza filhote😶‍🌫️</h1>
+  </div>
+  </center>
+  
+  
+  
+  
+  <body>
+    <style>
+      body {
+        background-image: url('https://eskipaper.com/images/adventure-time-wallpaper-31.jpg');
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+        height: 90vh;
+      }
+  
+  
+          .caixa1 {
+              background-color: rgba(9, 1, 1, 0.5);
+              display: flex;
+              justify-content: center;
+              padding: 20px;
+              border-radius: 50px;
+              box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+              color: #ff1100 ;
+              width: 500px;
+             
+             
+          }
+          .caixa2 {
+              background-color: rgba(9, 1, 1, 0.5);
+              display: flex;
+              justify-content: center;
+              padding: 20px;
+              border-radius: 10px;
+              box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+              color: #ff1100 ;
+              width: 250px;
+             
+             
+          }
+          .letra {
+            color:#fac800;
+            font-size:30px;
+            font-weight: bold;
+           
+           
+           
+  
+  
+          }
+  
+  
+  
+  
+  
+  
+    </style>
+  </body>
+  
