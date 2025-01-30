106a9412d80df53cd3cf4d62919c76a2b6e7aeed
fraancesss <guilhermefranca438@gmail.com>
2025-01-30 04:14:16 -0400

    multiverso-3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,22 @@
 <center>
   <div class="caixa1">
    
-  <h1>OLAAA, Seja bem-Vindo filhote😶‍🌫️</h1>
+  <h1>OLAAA, Seja bem-Vindo filhote😶‍🌫️
+
+  </h1>
    
   
 </div>
 </center>
+
+<center>
+  <div class="caixa3">
+    <div class="letra">
+  <h3> olhos sempre aberto arthur.,.,,.,...,,entao arraste para abaixo</h3>  
+  </div>  
+  </div>
+  </center>
+<br><br><br><br><br><br><br><br>
 
 <center>
 <div class="caixa2">
@@ -47,17 +58,28 @@
               padding: 20px;
               border-radius: 10px;
               box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-              color: #ff1100 ;
-              width: 250px;
-             
-             
-          }
-          .letra {
-            color:#fac800;
-            font-size:18px;
+              color: #fac800 ;
+              width: 100px;
+              font-size:18px;
             font-weight: bold;
-        
+            
           }
+              .caixa3 {
+              background-color: rgba(9, 1, 1, 0.5);
+              display: flex;
+              justify-content: center;
+              padding: 20px;
+              border-radius: 10px;
+              box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+              color: #55ff00 ;
+              width: 500px;
+              font-size:18px;
+            font-weight: bold;
+            
+             
+             
+          }
+          
   
     </style>
   </body>
